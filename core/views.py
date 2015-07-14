df63724fe4af0f52e252bb83f12c1bcafcb6178c
dm <notovet@gmail.com>
2015-07-14 16:19:08 -0400

    Add dash skeleton

--- core/views.py
+++ core/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.context_processors import csrf
+from towards.towds.models import Lister
+
 
 def Home(request):
+    template_name = core/dash.html
 
-    return HttpResponse("Hello towds")
+    user = None
+    if request.user.is_authenicated():
+        user = request.user
+
+    else:
+        return HttpResponse("Error: not logged in")
+
+
+    context = {'user': user}
+
+     
+    return render(request, template_name, context)
--- profile/models.py
+++ profile/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 from django.conf import settings
 
+from django.contrib.auth.models import User
+
 from django_extensions.db.models import TimeStampedModel
 from towds.ads.model import Address
 
 class Lister(TimeStampedModel):
+    user = models.ForeignKey(User, null=True, blank=True, default=None)
     name = models.CharField(max_length=255)
     email = models.EmailField(verbose_name='email address', max_length=255)
     home_address = models.ForeignKey(Address)
